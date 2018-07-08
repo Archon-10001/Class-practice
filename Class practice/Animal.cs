88b5393d8b4a506456387746bbb3eaa3dda73b7d
Casey <caseycjb007@gmail.com>
2018-07-08 21:15:40 +0100

    Initial commit

---
+++ Class practice/Animal.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_practice
+{
+    class Animal
+    {
+        //Class vaiables
+        public static int Count;
+        public static string name;
+        public static int age;
+        public static float happiness;
+
+        //Class constructors
+        public Animal(string _name, int _age, float _happiness)
+        {
+            name = _name;
+            age = _age;
+            happiness = _happiness;
+            Count++;
+        }
+        //Class methods
+        public void print()
+        {
+            Console.WriteLine("Name " + name);
+            Console.WriteLine("age " + age);
+            Console.WriteLine("Happiness" + happiness);
+        }
+
+    }
+}
---
+++ Class practice/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal dog = new Animal("Spotty", 19, 0.7f);
+            dog.print();
+
+            Console.WriteLine();
+
+            Animal cat = new Animal("Pat", 19, 0.5f);
+            cat.print();
+
+            Console.WriteLine();
+            Console.WriteLine("Total number of animals: " + Animal.Count);
+            Console.ReadLine();
+        }
+    }
+}
